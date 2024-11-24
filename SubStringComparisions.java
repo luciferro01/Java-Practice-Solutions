d29d27055bc291a8e4352500ad34756197d412df
Mohil Bansal <lucifer1111ronaldo@gmail.com>
2024-11-24 12:03:06 +0530

    HackerRank (Java) : 25(Java SubString Comparisions)

---
+++ SubStringComparisions.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class SubStringComparisions {
+
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = s.substring(0, k);
+        String largest = s.substring(0, k);
+
+        for (int i = 0; i <= s.length() - k; i++) {
+            String subStr = s.substring(i, i + k);
+            if (smallest.compareTo(subStr) > 0) {
+                smallest = subStr;
+            }
+            if (largest.compareTo(subStr) < 0) {
+                largest = subStr;
+            }
+        }
+
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length 'k'
+
+        return smallest + "\n" + largest;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
